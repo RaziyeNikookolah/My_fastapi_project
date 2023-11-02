180d5f70f38e586003a4a6c1f1feabd301eead76
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-11-02 10:36:06 +0330

    password verification method

--- models.py
+++ models.py
@@ -15,6 +15,9 @@ class UserModel(database.Base):
     created_at=sqlalchemy.Column(sqlalchemy.DateTime,default=datetime.datetime.utcnow())
     posts=orm.relationship("Post",back_populates="users")
     
+    def password_verification(self,password:str):
+        return hash.bcrypt.verify(password,self.password_hash)
+    
 class PostModel(database.Base):
     __tablename__="posts"
     id=sqlalchemy.Column(sqlalchemy.Integer,primary_key=True,index=True)
@@ -23,3 +26,5 @@ class PostModel(database.Base):
     post_description=sqlalchemy.Column(sqlalchemy.String)
     created_at=sqlalchemy.Column(sqlalchemy.DateTime,default=datetime.datetime.utcnow())
     users=orm.relationship("User",back_populates="posts")    
+    
+    
