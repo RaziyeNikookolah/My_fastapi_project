3d6536623ab430a385b0373872bac2791ac281ac
RaziyeNikookolah <r.nikookolah@gmail.com>
2023-11-02 10:29:06 +0330

    initial user and post models

---
+++ database.py
@@ -0,0 +1,21 @@
+import sqlalchemy
+import sqlalchemy.ext.declarative as declarative
+import sqlalchemy.orm as orm
+
+DB_URL="sqlite:///./dbfile.db"
+
+"""
+connect_args={"check_same_thread"} is a parameter used when creating a SQLAlchemy database engine for SQLite. This parameter is specific to SQLite and controls how SQLite handles multiple threads accessing the same database connection.
+
+SQLite is designed as a serverless, self-contained database engine, and by default, it doesn't support concurrent access from multiple threads. 
+If you try to use a single SQLite database connection from multiple threads simultaneously, it can lead to issues or errors.
+
+"""
+
+engine=sqlalchemy.create_engin(DB_URL,connect_args={"check_same_thread":False})
+
+SessionLocal = orm.sessionmaker(autocommit=False,autoflush=False,bind=engine)
+"""
+By defining models based on the Base class, you create a structured and consistent way to work with your database tables using Python objects and SQLAlchemy's ORM features.
+"""
+Base = declarative.declarative_base()
---
+++ models.py
@@ -0,0 +1,25 @@
+import sqlalchemy
+import sqlalchemy.orm as orm
+import passlib.hash as hash
+import datetime
+
+import database 
+
+class UserModel(database.Base):
+    __tablename__="users"
+    id=sqlalchemy.Column(sqlalchemy.Integer,primary_key=True,index=True)
+    email = sqlalchemy.Column(sqlalchemy.String,unique=True,index=True)
+    name=sqlalchemy.Column(sqlalchemy.String)
+    phone=sqlalchemy.Column(sqlalchemy.String)
+    password_hash=sqlalchemy.Column(sqlalchemy.String)
+    created_at=sqlalchemy.Column(sqlalchemy.DateTime,default=datetime.datetime.utcnow())
+    posts=orm.relationship("Post",back_populates="users")
+    
+class PostModel(database.Base):
+    __tablename__="posts"
+    id=sqlalchemy.Column(sqlalchemy.Integer,primary_key=True,index=True)
+    user_id=sqlalchemy.Column(sqlalchemy.Integer,sqlalchemy.ForeignKey("users.id"))
+    post_title=sqlalchemy.Column(sqlalchemy.String,index=True)
+    post_description=sqlalchemy.Column(sqlalchemy.String)
+    created_at=sqlalchemy.Column(sqlalchemy.DateTime,default=datetime.datetime.utcnow())
+    users=orm.relationship("User",back_populates="posts")    
